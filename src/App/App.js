54c234378b317b70cca74c38b9cd6f1721e50a05
Beth Wilson <bethwilson777@gmail.com>
2022-10-23 12:24:31 -0600

    Hide the search on single page

--- src/App/App.js
+++ src/App/App.js
@@ -62,12 +62,16 @@ class App extends Component {
             alt="Home icon to take user back to main view" onClick={() => this.clearInputs()}/></NavLink>
           </nav>
           <main className="App">
-            <Search searchForMovie={this.searchForMovie} />
             {this.state.loading && <p className="apple-loader-text"> Loading your sour apples...</p>}
             {this.state.loading && <img src={apple} className="apple-loader" alt="apple loader" />}
             {this.state.error && <h3>Oops, that was a bad apple, please try again!</h3> }
            
-            <Route exact path='/' render={() =>  <Movies movies={this.state.movies} showSingleMovie={this.showSingleMovie} searchMovie={this.state.searchMovie}/> } />
+            <Route exact path='/' render={() =>  ( 
+              <div>
+                <Search searchForMovie={this.searchForMovie} />
+                <Movies movies={this.state.movies} showSingleMovie={this.showSingleMovie} searchMovie={this.state.searchMovie}/>  
+              </div> )}
+              />
             <Route exact path='/:id' render={({ match }) => <SingleMovie id={match.params.id}/> }  />
           </main>
           <footer>
